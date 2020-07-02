39cd6e0ec37225dc9cd7399da1787f63f0598583
Jaloliddin Mirzo <67505308+jmbots@users.noreply.github.com>
2020-07-02 21:35:34 +0500

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,46 @@
+class Translation(object):
+    START_TEXT = """Assalomu alaykum,
+bu men renameuzsbot qiluvchi bot @userbotp tomindan yaratilgan!
+
+<b>Menga biron bir telegram file yuboring va replay qilib /rename buyrug`ini berib kerakli `nom`ni bering</b>
+
+/help barcha tafsilotlar.."""
+    RENAME_403_ERR = "Kechirasiz. Ushbu faylning nomini o'zgartirishga ruxsat yo'q."
+    ABS_TEXT = " Iltimos, hadingizdan oshmang."
+    UPGRADE_TEXT = "<b>👉 O'zingiz uchun renameuzsbot Clone Bot yarating... </b>  /help buyrug`i batafsilroq..."
+    DOWNLOAD_START = "yuklab olinmoqda sabr.."
+    UPLOAD_START = "Vav yuklanmoqda `upload`"
+    RCHD_TG_API_LIMIT = "shu soniya ichida {} yuklab olindi.\nDetected File hajmi: {}\nSorry. <@userbotp> Ammo, men Telegram API cheklovlari tufayli 1.5 Gb dan katta fayllarni yuklay olmayman."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Mendan foydalanayotganingiz uchun rahmat🤓.**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "shu soniya ichida {} yuklab olindi.\nUploaded yuborildi {} shu soniya ichida."
+    NOT_AUTH_USER_TEXT = "Iltimos shu buyruq bilan obuna mudatini yangilang /upgrade."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Aniqlangan fayl hajmi: {}. Oddiy foydalanuvchi foydalana oladi: {}\nPlease /upgrade Shu buyruq bilan pro foydalanuvchi bo`ing.\nIf agar xato topsangiz bizga murojat qiling: <a href='https://telegram.dog/@mirzo_jaloliddin'>@jmanonym</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Sizning faylininguz uchun logo eskez saqlandi. Ushbu rasm faylda ishlatiladi."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Sizning rasmingiz muvafaqiyatli o`chirildi."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media muvaffaqiyatli o'chirildi."
+    SAVED_RECVD_DOC_FILE = "Hujjat muvaffaqiyatli yuklandi."
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "Haqiqiy ThumbNail topilmadi."
+    USER_ADDED_TO_DB = "Foydalanuvchi <a href='tg://user?id={}'>{}</a> qo`shildi {} havolaga {}."
+    HELP_USER = """Assalomu alaykum bu men renameuzsbot..
+    
+1. Menga Telegram file yuboring.
+2. Yuborgan fileni o`zgartirish uchun uni replay qilib /rename buyrugini bering, shundan so`ng kerakli nomni yozing..
+   
+<b>👉 Botni ochiq code olish uchun :</b> 👉 <a href="telegram.me/userbotp">UserBotP</a>
+
+--------
+
+Qo`llab quvatlash : @userbotp
+© @Mirzo_Jaloliddin """
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Media ustiga logo eskez qoyish uchun birinchi logoni yuboring keyin esa video yoki shunga oxshash media jonating va unga replay qilib shu buyruqni bering `/rename buyrug`ini bergach ozingiz hohlagan nomni yozing .."
+    ABUSIVE_USERS = "Ushbu botni ishlatishingiz uchun ruxsat yo`q. nega deb hayron bo`layotgan bo`lsangiz /me shu buyruq bilan xatoni bilib oling / balkim siz kanalimizga a`zo emasdirsiz bu esa mumkin emas."
+    FREE_USER_LIMIT_Q_SZE = """O`zgartirib bo`lmadi.
+Oddiy foydalanuvchilar 30 daqiqada atigi bitta so'rov jo'nata oladi pro user qiling.
+/upgrade shu buyruq bilan pro qiling yoki 1800 sekundan so'ng urinib ko'ring."""
+    IFLONG_FILE_NAME = """Telegram tomonidan ruxsat etilgan fayl nomining chegarasi {alimit} belgidan iborat.
+Yuborilgan file nomida  {num} shuncha belgi mavjud.
+
+<b>Telegramda file nomini uzun qila olmaysiz va turlicha ishora qo'yishni kamaytiring!</b>
+©️ <code>@renameuzsbot</code>
+Fayl nomini qisqartiring va qaytadan urinib ko'ring!"""
